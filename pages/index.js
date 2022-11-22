e711196ecc2734ad60e46992eda203ff52cbbf89
Hamid <www.abdulhamid3311@gmail.com>
2022-11-22 12:25:54 +0600

    Project start

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className='text-3xl text-center font-bold'> Hello</h2>
+    </div>
+  )
+}
